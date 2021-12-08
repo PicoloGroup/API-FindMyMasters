84a152d1a079ed58ddb96e1a92738884bfa9a3aa
Fırat Tamur <ftamur16@ku.edu.tr>
2021-12-09 00:28:05 +0300

    test: refactor mock user

--- test/utils/mock-user.ts
+++ test/utils/mock-user.ts
@@ -4,6 +4,7 @@ import { AuthUser } from '../../src/auth/auth-user';
 export const mockUser = (fields?: Partial<AuthUser>): AuthUser => ({
   registrationDate: new Date(),
   email: 'ftamur16@ku.edu.tr',
+  username: null,
   role: Role.STUDENT,
   id: 1,
   emailVerified: true,
